834a5ccc62abf18c944fc2a78886e66d37fc54f3
joylantz <jelantz1@unm.edu>
2025-05-04 22:23:39 -0600

    Done for tonight. Added function for creating sliders

--- app/Display.hs
+++ app/Display.hs
@@ -5,10 +5,14 @@ import Equations
 import Orbit
 import Graphics.Gloss.Juicy
 
-createPlanetButtons :: UI -> [Planet] -> IO [(Planet, Button Picture)]
-createPlanetButtons ui planets = mapM (makeButton ui) planets
-    where
-        makeButton :: UI -> Planet -> IO (Planet, ButtonPicture)
+--TODO:
+--Create event handlers to handle button clicks and sliding of sliders
+--Implement these functions
+--Update orbit information based on user selections
+
+--Would like to keep as much as possible in Display rather than Main
+
+makeButton :: UI -> Planet -> IO (Planet, ButtonPicture)
         makeButton ui planet do
             planetPic <- loadJuicyPNG (planetImage planet)
             let image = case planetPic of
@@ -16,3 +20,13 @@ createPlanetButtons ui planets = mapM (makeButton ui) planets
                 Nothing -> color red (text "Error")
             button <- button ui image
             return (planet, button)
+
+createPlanetButtons :: UI -> [Planet] -> IO [(Planet, Button Picture)]
+createPlanetButtons ui planets = mapM (makeButton ui) planets
+
+createSliders :: UI -> IO (Slider float, Slider float, Slider float)
+createSliders ui = do
+    velSlider <- slider ui (0, 10) 5 --FIX: Not sure exactly what these values should be
+    altSlider <- slider ui (0, 10) 5 --FIX
+    eccSlider <- slider ui (0, 10) 5 --FIX
+    return (velSlider, altSlider, eccSlider)
